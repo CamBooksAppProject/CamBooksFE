f537ed5362953e92e72d943e8bed920f6ed633e4
jungbee2 <126428632+jungbee2@users.noreply.github.com>
2025-06-18 00:03:44 +0900

    restyle up

--- CamBooksApp/App.js
+++ CamBooksApp/App.js
@@ -1,71 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import SplashScreen from './src/Screens/SplashScreen';
-import RouteScreen from './src/Navigation/RouteScreen';
-import LoginScreen from './src/Screens/LoginScreen';
-import SignUpScreen from './src/Screens/SignUpScreen';
-import AuthenticationScreen from './src/Screens/AuthenticationScreen';
-import GuestScreen from './src/Screens/GuestScreen';
+import SplashScreen from "./src/Screens/SplashScreen";
+import RouteScreen from "./src/Navigation/RouteScreen";
+import LoginScreen from "./src/Screens/LoginScreen";
+import SignUpScreen from "./src/Screens/SignUpScreen";
+import AuthenticationScreen from "./src/Screens/AuthenticationScreen";
+import GuestScreen from "./src/Screens/GuestScreen";
 
+import HomeScreen from "./src/Screens/HomeScreen";
+import CommunityScreen from "./src/Screens/CommunityScreen";
+import ChatScreen from "./src/Screens/ChatScreen";
+import ScrapScreen from "./src/Screens/ScrapScreen";
+import ProfileScreen from "./src/Screens/ProfileScreen";
 
-import HomeScreen from './src/Screens/HomeScreen';
-import CommunityScreen from './src/Screens/CommunityScreen';
-import ChatScreen from './src/Screens/ChatScreen';
-import ScrapScreen from './src/Screens/ScrapScreen';
-import ProfileScreen from './src/Screens/ProfileScreen';
+import CommnuityPage from "./src/Screens/Pages/CommunityPage";
+import FreeBoardPage from "./src/Screens/Pages/FreeBoardPage";
 
-import CommnuityPage from './src/Screens/Pages/CommunityPage';
-import FreeBoardPage from './src/Screens/Pages/FreeBoardPage';
+import CommunityScrapPage from "./src/Screens/Pages/CommunityScrapPage";
+import HomeScreenScrapPage from "./src/Screens/Pages/HomeScreenScrapPage";
+import FreeBoardScrapPage from "./src/Screens/Pages/FreeBoardScrapPage";
 
-import CommunityScrapPage from './src/Screens/Pages/CommunityScrapPage';
-import HomeScreenScrapPage from './src/Screens/Pages/HomeScreenScrapPage';
-import FreeBoardScrapPage from './src/Screens/Pages/FreeBoardScrapPage';
+import HomePostPage from "./src/Screens/Pages/HomePostPage";
+import CommuPostPage from "./src/Screens/Pages/CommuPostPage";
+import FreeBoardPostPage from "./src/Screens/Pages/FreeBoardPostPage";
 
-import HomePostPage from './src/Screens/Pages/HomePostPage';
-import CommuPostPage from './src/Screens/Pages/CommuPostPage';
-import FreeBoardPostPage from './src/Screens/Pages/FreeBoardPostPage';
+import HomeDetailPage from "./src/Screens/Pages/HomeDetailPage";
+import CommuDetailPage from "./src/Screens/Pages/CommuDetailPage";
+import FreeBoardDetailPage from "./src/Screens/Pages/FreeBoardDetailPage";
+import ChatDetailPage from "./src/Screens/Pages/ChatDetailPage";
 
-import HomeDetailPage from './src/Screens/Pages/HomeDetailPage';
-import CommuDetailPage from './src/Screens/Pages/CommuDetailPage';
-import FreeBoardDetailPage from './src/Screens/Pages/FreeBoardDetailPage';
-import ChatDetailPage from './src/Screens/Pages/ChatDetailPage';
-
-import SearchingPage from './src/Screens/Pages/SearchingPage';
-import NotificationPage from './src/Screens/Pages/NotificationPage';
-import SettingPage from './src/Screens/Pages/SettingPage';
-import SettingModal from './src/Screens/Pages/SettingModal';
-
+import SearchingPage from "./src/Screens/Pages/SearchingPage";
+import NotificationPage from "./src/Screens/Pages/NotificationPage";
+import SettingPage from "./src/Screens/Pages/SettingPage";
+import SettingModal from "./src/Screens/Pages/SettingModal";
 
 import FindId from "./src/Screens/Pages/FindId";
 import FindPw from "./src/Screens/Pages/FindPw";
 import NoticePage from "./src/Screens/Pages/NoticePage";
 import ChangePw from "./src/Screens/Pages/ChangePw";
+import ChangeAddr from "./src/Screens/Pages/ChangeAddr";
+
 import MyInfo from "./src/Screens/Pages/MyInfo";
 import MyPost from "./src/Screens/Pages/MyPost";
 
+import SalesHistory from "./src/Screens/Pages/SalesHistory";
+import PurchaseHistory from "./src/Screens/Pages/PurchaseHistory";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   const [isShowSplash, setIsShowSplash] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setIsShowSplash(false);
     }, 1000);
-  })
+  });
 
   return (
     <>
-      {isShowSplash ? (<SplashScreen />) : (
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : (
         <GestureHandlerRootView style={{ flex: 1 }}>
           <NavigationContainer>
             <Stack.Navigator initialRouteName="LoginScreen">
-
               <Stack.Screen
                 name="RouteScreen"
                 component={RouteScreen}
@@ -109,6 +111,11 @@ export default function App() {
               />
 
               <Stack.Screen
+                name="ChangeAddr"
+                component={ChangeAddr}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
                 name="MyInfo"
                 component={MyInfo}
                 options={{ headerShown: false }}
@@ -117,6 +124,18 @@ export default function App() {
               <Stack.Screen
                 name="MyPost"
                 component={MyPost}
+                options={{ headerShown: false }}
+              />
+
+              <Stack.Screen
+                name="SalesHistory"
+                component={SalesHistory}
+                options={{ headerShown: false }}
+              />
+
+              <Stack.Screen
+                name="PurchaseHistory"
+                component={PurchaseHistory}
                 options={{ headerShown: false }}
               />
 
@@ -202,17 +221,20 @@ export default function App() {
                 options={{ headerShown: false }}
               />
 
-              <Stack.Screen name="HomeDetailPage"
+              <Stack.Screen
+                name="HomeDetailPage"
                 component={HomeDetailPage}
                 options={{ headerShown: false }}
               />
 
-              <Stack.Screen name="CommuDetailPage"
+              <Stack.Screen
+                name="CommuDetailPage"
                 component={CommuDetailPage}
                 options={{ headerShown: false }}
               />
 
-              <Stack.Screen name="FreeBoardDetailPage"
+              <Stack.Screen
+                name="FreeBoardDetailPage"
                 component={FreeBoardDetailPage}
                 options={{ headerShown: false }}
               />
@@ -252,11 +274,10 @@ export default function App() {
                 component={SettingModal}
                 options={{ headerShown: false }}
               />
-
             </Stack.Navigator>
           </NavigationContainer>
         </GestureHandlerRootView>
       )}
     </>
   );
-};
+}
